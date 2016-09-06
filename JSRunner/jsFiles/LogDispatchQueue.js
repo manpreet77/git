ef5e6288b71bdaea088475450f0bb0a6769d046d
Shridhar <Shridhar@SV-Spectre>
2016-09-06 13:09:04 +0530

    V1.6.1 Added EventETA, PrepareETA Added Arr SLA Breach Handling

--- JSRunner/jsFiles/LogDispatchQueue.js
+++ JSRunner/jsFiles/LogDispatchQueue.js
@@ -8,10 +8,10 @@
 */
 Log.info("Log DispatchQueue Entering................................................");
 var DispatchQueue = (Workflow.DispatchQueueStringify !== 'undefined' ? JSON.parse (Workflow.DispatchQueueStringify): 'undefined');
-Log.info ('EventType       SendTime                     DelayMins  Status  Channel ContactType     Level   AtmSchedule             FirstName   LastName    Address             FirstName2  LastName2   Address2            Content     Template        WillRespond     Ttl     MaxRetries  TryCount       ');
+Log.info ('EventType       SendTime                     DelayMins  Status  Channel ContactType     Level   AtmSchedule             WillRespond     Ttl     MaxRetries  TryCount    FirstName   LastName    Address         FirstName2  LastName2   Address2        Content         Template         ');
 for (var i in DispatchQueue) {
     var dq = DispatchQueue[i];
-    Log.info (dq.EventType     + "\t\t" +dq.SendTime      + "\t" +dq.DelayMins     + "\t" +dq.Status        + "\t" +dq.Channel       + "\t" +dq.ContactType   + "\t" +dq.Level+ "\t" +         dq.AtmSchedule+ "\t" +   dq.FirstName     + "\t" +dq.LastName      + "\t\t" +dq.Address       + "\t" +dq.FirstName2    + "\t\t" +dq.LastName2     + "\t\t" +dq.Address2      + "\t" +dq.Content       + "\t" +dq.Template      + "\t" +dq.WillRespond   + "\t\t\t\t" +dq.Ttl           + "\t" +dq.MaxRetries    + "\t\t\t" +dq.TryCount      );
+    Log.info (dq.EventType     + "\t\t" +dq.SendTime      + "\t" +dq.DelayMins     + "\t" +dq.Status        + "\t" +dq.Channel       + "\t" +dq.ContactType   + "\t" +dq.Level+ "\t" +         dq.AtmSchedule+ "\t" +dq.WillRespond   + "\t\t" +dq.Ttl           + "\t" +dq.MaxRetries    + "\t\t" +dq.TryCount +      "\t" +   dq.FirstName     + " " +dq.LastName      + " " +dq.Address       + "\t\t" +dq.FirstName2    + " " +dq.LastName2     + " " +dq.Address2      + "\t\t" +dq.Content       + "\t" +dq.Template      );
 }
 Log.info("Log DispatchQueue Exitting ...............................................");
 // --------------------------------------------------------------------------------
